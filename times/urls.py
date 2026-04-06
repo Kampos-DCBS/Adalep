d67f1be04140e5b67403274f8914cc9ea70ba05c
Daniel Campos <danielcamposborgessantiago6@gmail.com>
2026-04-06 13:38:10 -0300

    opção de editar e excluir e adicão de bootstrap

--- times/urls.py
+++ times/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.lista_times, name='lista_times'),
+    path('editar/<int:id>/', views.editar_time, name='editar_time'),
+    path('excluir/<int:id>/', views.excluir_time, name='excluir_time'),
 ]
--- times/views.py
+++ times/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Time
 
 def lista_times(request):
     times = Time.objects.all()
     return render(request, 'times/lista.html', {'times': times})
+
+
+def editar_time(request, id):
+    time = get_object_or_404(Time, id=id)
+
+    if request.method == 'POST':
+        time.nome = request.POST.get('nome')
+        time.cidade = request.POST.get('cidade')
+        time.fundacao = request.POST.get('fundacao')
+        time.save()
+        return redirect('lista_times')
+
+    return render(request, 'times/editar.html', {'time': time})
+
+
+def excluir_time(request, id):
+    time = get_object_or_404(Time, id=id)
+    time.delete()
+    return redirect('lista_times')
